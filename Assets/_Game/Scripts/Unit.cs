9d13bf8ee9313536ecfb65692bfc16dcad72b2fd
OrAdzia <oradzia.work@gmail.com>
2022-06-27 20:18:50 +0300

    Made a unit selection + visual selection

--- Assets/_Game/Scripts/Unit.cs
+++ Assets/_Game/Scripts/Unit.cs
@@ -4,25 +4,38 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+    [SerializeField] private Animator unitAnimator;
+
     private Vector3 targetPosition;
+
+    private void Awake() 
+    {
+        targetPosition = transform.position;
+    }
 
     private void Update()
     {
         float stoppingDistance = 0.1f;
+
         if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
         {
             Vector3 moveDirection = (targetPosition - transform.position).normalized;
+
             float moveSpeed = 4f;
             transform.position += moveDirection * moveSpeed * Time.deltaTime;
-        }
 
-        if (Input.GetMouseButtonDown(0))
+            float rotateSpeed = 10f;
+            transform.forward = Vector3.Lerp(transform.forward, moveDirection, rotateSpeed * Time.deltaTime);
+
+            unitAnimator.SetBool("IsWalking", true);
+        }
+        else
         {
-            Move(MouseWorld.GetPosition());
+            unitAnimator.SetBool("IsWalking", false);
         }
     }
 
-    private void Move(Vector3 targetPosition)
+    public void Move(Vector3 targetPosition)
     {
         this.targetPosition = targetPosition;
     }
