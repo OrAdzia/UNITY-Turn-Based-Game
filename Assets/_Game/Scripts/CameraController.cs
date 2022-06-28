1593e34f6cd4589a92f45ecd8fd83c6b1a6ce72c
OrAdzia <oradzia.work@gmail.com>
2022-06-28 03:09:09 +0300

    Added a camera zoom + cleaned code

--- Assets/_Game/Scripts/CameraController.cs
+++ Assets/_Game/Scripts/CameraController.cs
@@ -1,10 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class CameraController : MonoBehaviour
 {
+    [SerializeField] private CinemachineVirtualCamera cinemachineVirtualCamera;
+
+    private const float MIN_FOLLOW_Y_OFFSET = 2f;
+    private const float MAX_FOLLOW_Y_OFFSET = 12f;
+    private Vector3 targetFollowOffset;
+    private CinemachineTransposer cinemachineTransposer;
+
+    private void Start()
+    {
+        cinemachineTransposer = cinemachineVirtualCamera.GetCinemachineComponent<CinemachineTransposer>();
+        targetFollowOffset = cinemachineTransposer.m_FollowOffset;
+    }
+
     private void Update()
+    {
+        HandleMovement();
+        HandleRotation();
+        HandleZoom();
+    }
+
+    private void HandleMovement()
     {
         Vector3 inputMoveDir = new Vector3(0f, 0f, 0f);
 
@@ -29,7 +50,10 @@ public class CameraController : MonoBehaviour
 
         Vector3 moveVector = transform.forward * inputMoveDir.z + transform.right * inputMoveDir.x;
         transform.position += moveVector * moveSpeed * Time.deltaTime;
+    }
 
+    private void HandleRotation()
+    {
         Vector3 rotationVector = new Vector3(0f, 0f, 0f);
 
         if (Input.GetKey(KeyCode.Q))
@@ -43,5 +67,22 @@ public class CameraController : MonoBehaviour
 
         float rotationSpeed = 100f;
         transform.eulerAngles += rotationVector * rotationSpeed * Time.deltaTime;
+    }
+
+    private void HandleZoom()
+    {
+        float zoomAmount = 1f;
+        if (Input.mouseScrollDelta.y > 0)
+        {
+            targetFollowOffset.y -= zoomAmount;
+        }
+        if (Input.mouseScrollDelta.y < 0)
+        {
+            targetFollowOffset.y += zoomAmount;
+        }
+
+        float zoomSpeed = 5f;
+        targetFollowOffset.y = Mathf.Clamp(targetFollowOffset.y, MIN_FOLLOW_Y_OFFSET, MAX_FOLLOW_Y_OFFSET);
+        cinemachineTransposer.m_FollowOffset = Vector3.Lerp(cinemachineTransposer.m_FollowOffset, targetFollowOffset, Time.deltaTime * zoomSpeed);
     }
 }
